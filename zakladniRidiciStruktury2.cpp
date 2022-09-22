75cf55c0b7a9d2c04865575c5cacca6d2dca78d7
brenekv.03 <brenekv.03@A321-15>
2022-09-22 11:30:49 +0200

    Přidejte soubory projektu.

---
+++ zakladniRidiciStruktury2.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	srand(time(NULL));
+	cout << "Zadejte cislo n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		double nahodneCislo=
+	}
+}
+
