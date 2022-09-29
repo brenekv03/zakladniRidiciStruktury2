a2320c659befc485f41b2020437672b4ba6e8ba1
brenekv.03 <brenekv.03@A321-15>
2022-09-29 10:28:41 +0200

    hotovo

--- zakladniRidiciStruktury2.cpp
+++ zakladniRidiciStruktury2.cpp
@@ -1,15 +1,66 @@
 ﻿
 #include <iostream>
+#include <ctime>
 using namespace std;
 int main()
 {
 	int n;
-	srand(time(NULL));
+	srand((unsigned)time(NULL));
 	cout << "Zadejte cislo n: ";
 	cin >> n;
-	for (int i = 0; i < n; i++)
+	double soucetPrvku = 0;
+	cout << "\na-Cyklem for\nb-Cyklem while\nc-Cyklem do while\nk-Konec\nZvolte si cim chcete pocitat: ";
+	char volba;
+	cin >> volba;
+	while (volba != 'k')
 	{
-		double nahodneCislo=
+		soucetPrvku = 0;
+		switch (volba)
+		{
+		case 'a':
+		{
+			for (int i = 0; i < n; i++)
+			{
+				double nahodneCislo = 10 + (100 - 10) * (double)rand() / (RAND_MAX + 1);
+				soucetPrvku += nahodneCislo;
+			}
+			cout << "\nSoucet vygenerovanych je: " << soucetPrvku;
+		}break;
+		case 'b':
+		{
+			int i = 0;
+			while (i < n)
+			{
+				double nahodneCislo = 10 + (100 - 10) * (double)rand() / (RAND_MAX + 1);
+				soucetPrvku += nahodneCislo;
+				i++;
+			}
+			cout << "\nSoucet vygenerovanych je: " << soucetPrvku;
+		}break;
+		case 'c':
+		{
+			int i = 0;
+			do
+			{
+				double nahodneCislo = 10 + (100 - 10) * (double)rand() / (RAND_MAX + 1);
+				soucetPrvku += nahodneCislo;
+				i++;
+			} while (i < n);
+			cout << "\nSoucet vygenerovanych je: " << soucetPrvku;
+		}
+		case 'k':
+		{
+			cout << "Zvolili jste konec";
+		}
+		default:
+		{
+			cout << "\nSpatna volba";
+		}break;
+		}
+		cout << "Zadejte cislo n: ";
+		cin >> n;
+		cout << "a-Cyklem while\nb-Cyklem do while\nc-Cyklem for\nk-Konec\nZvolte si cim chcete pocitat: ";
+		cin >> volba;
 	}
 }
 
